af841d8ebc6352a483c2a9ebc30efd87b27e37c1
wsspence <wsspence@ncsu.edu>
2017-09-12 12:47:33 -0400

    changed newlines and tabs to spaces

--- p2/magic.c
+++ p2/magic.c
@@ -10,91 +10,91 @@
 #define RESET "\033[0m"
 
 bool isDigit( char ch ) {
-	//check if param is a digit
-	if ( ch >= '0' && ch <= '9' ) {
-		return true;
-	}
-	
-	return false;
+    //check if param is a digit
+    if ( ch >= '0' && ch <= '9' ) {
+        return true;
+    }
+    
+    return false;
 }
 
 bool isAlpha( char ch ) {
-	//check if param is a letter
-	if ( ch >= 'a' && ch <= 'z' ) {
-		return true;
-	}
-	else if ( ch >= 'A' && ch <= 'Z' ) {
-		return true;
-	}
-	return false;
+    //check if param is a letter
+    if ( ch >= 'a' && ch <= 'z' ) {
+        return true;
+    }
+    else if ( ch >= 'A' && ch <= 'Z' ) {
+        return true;
+    }
+    return false;
 }
 
 void highlightNumber() {
-	//if token is an integer, highlight it
-	char ch = getchar(); 
-	
-	//change print color to red
-	printf( RED );
-	
-	//print the digit (since it's back on the stream)
-	while ( isDigit( ch ) ) {
-		printf( "%c", ch );
-		ch = getchar();
-	}
-	//change color back to default
-	printf( RESET );
-	
-	//print the next char (if it's not an EOF)
-	if ( ch != EOF ) {
-		printf( "%c", ch );
-	}
+    //if token is an integer, highlight it
+    char ch = getchar(); 
+    
+    //change print color to red
+    printf( RED );
+    
+    //print the digit (since it's back on the stream)
+    while ( isDigit( ch ) ) {
+        printf( "%c", ch );
+        ch = getchar();
+    }
+    //change color back to default
+    printf( RESET );
+    
+    //print the next char (if it's not an EOF)
+    if ( ch != EOF ) {
+        printf( "%c", ch );
+    }
 }
 
 void skipIdentifier() {
-	//if token is identifier, skip highlighting any digits in this token (move stream to next char after token)
-	char ch = getchar();
-	
-	//loop, printing the chars of the token, until the end of the token 
-	//(excluding tabs since Jenkins tells me that's wrong)
-	while ( isAlpha( ch ) || isDigit( ch ) || ch == '_' ) {
-		printf( "%c", ch );
-		ch = getchar();
-	}
-	
-	//print the next char (if it's not an EOF)
-	if ( ch != EOF ) {
-		printf( "%c", ch );
-	}
+    //if token is identifier, skip highlighting any digits in this token (move stream to next char after token)
+    char ch = getchar();
+    
+    //loop, printing the chars of the token, until the end of the token 
+    //(excluding tabs since Jenkins tells me that's wrong)
+    while ( isAlpha( ch ) || isDigit( ch ) || ch == '_' ) {
+        printf( "%c", ch );
+        ch = getchar();
+    }
+    
+    //print the next char (if it's not an EOF)
+    if ( ch != EOF ) {
+        printf( "%c", ch );
+    }
 }
 
 
 int main() {
-	char ch;
-	
-	//read user input one char at a time
-	ch = getchar();
-	while ( ch != EOF ) {
-		//check if ch is the beginning of an identifier (letter or underscore) 
-		if ( isAlpha( ch ) || ch == '_' ) {
-			//iterate through token
-			ungetc( ch, stdin );
-			skipIdentifier();
-		}
-		
-		//check if ch is a digit (that isn't part of an identifier)
-		else if ( isDigit( ch ) ) {
-			//put ch back on stream to be read again, then highlight it
-			ungetc( ch, stdin );
-			highlightNumber();
-		}
-		
-		//print the character
-		else {
-			printf( "%c", ch );
-		}
-		
-		ch = getchar();
-	}
-	
-	return 0;
+    char ch;
+    
+    //read user input one char at a time
+    ch = getchar();
+    while ( ch != EOF ) {
+        //check if ch is the beginning of an identifier (letter or underscore) 
+        if ( isAlpha( ch ) || ch == '_' ) {
+            //iterate through token
+            ungetc( ch, stdin );
+            skipIdentifier();
+        }
+        
+        //check if ch is a digit (that isn't part of an identifier)
+        else if ( isDigit( ch ) ) {
+            //put ch back on stream to be read again, then highlight it
+            ungetc( ch, stdin );
+            highlightNumber();
+        }
+        
+        //print the character
+        else {
+            printf( "%c", ch );
+        }
+        
+        ch = getchar();
+    }
+    
+    return 0;
 }
