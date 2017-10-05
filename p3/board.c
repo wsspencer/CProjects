3f46b71716468db5d23088adb62762797f1810e2
wsspence <wsspence@ncsu.edu>
2017-10-04 22:59:16 -0400

    waiiiiit

--- p3/board.c
+++ p3/board.c
@@ -124,7 +124,7 @@ bool moveUp( int tile, int rows, int cols, int board[][ cols ] ) {
     //If this doesn't change pointer vals, just go back to searching manually
     //check if tile is in board?
     if ( findTile(tile, rows, cols, board, &targRow, &targCol) == false ) {
-        printf("error message?"); //?
+        printf("Invalid command\n"); //?
         return false;
     }
 
@@ -151,7 +151,7 @@ bool moveDown( int tile, int rows, int cols, int board[][ cols ] ) {
     //If this doesn't change pointer vals, just go back to searching manually
     //check if tile is in board?
     if ( findTile(tile, rows, cols, board, &targRow, &targCol) == false ) {
-        printf("error message?"); //?
+        printf("Invalid command\n"); //?
         return false;
     }
     //save the value of the last element in target column before it's overwritten
@@ -177,7 +177,7 @@ bool moveLeft( int tile, int rows, int cols, int board[][ cols ] ) {
     //If this doesn't change pointer vals, just go back to searching manually
     //check if tile is in board?
     if ( findTile(tile, rows, cols, board, &targRow, &targCol) == false ) {
-        printf("error message?"); //?
+       printf("Invalid command\n"); //?
         return false;
     }
     //save the value of the first element in target row before it's overwritten
@@ -203,7 +203,7 @@ bool moveRight( int tile, int rows, int cols, int board[][ cols ] ) {
     //If this doesn't change pointer vals, just go back to searching manually
     //check if tile is in board?
     if ( findTile(tile, rows, cols, board, &targRow, &targCol) == false ) {
-        printf("error message?"); //?
+        printf("Invalid command\n"); //?
         return false;
     }
     //save the value of the last element in target column before it's overwritten
